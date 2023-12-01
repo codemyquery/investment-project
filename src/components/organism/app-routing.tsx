9fc12015ffd63d6792694d378fd177f8c5d52a1d
Ashutosh <ashutoshsingh5192344@gmail.com>
2023-12-01 08:07:23 +0530

    Fixed the routing path

--- src/components/organism/app-routing.tsx
+++ src/components/organism/app-routing.tsx
@@ -1,16 +1,45 @@
 import { Route, Routes } from "react-router-dom";
 import { NavigationMenu, Service, ServiceStatus } from "../../types"
-import { Login, PageNotFound } from "../pages";
+import { DashboardPlanDetails, HomePage, Login, PageNotFound } from "../pages";
 import { useAuth } from "../../providers";
 import { AdminLoginPage } from "../pages/admin/login";
 
 interface AppRoutingProps {
     routingService: Service<NavigationMenu[]>;
 }
+interface getRoutingProps {
+    route: NavigationMenu,
+    currentPath: string,
+    submenu: NavigationMenu[] | undefined
+}
+
 export const AppRouting = ({
     routingService
 }: AppRoutingProps) => {
     const { adminInfo, userInfo } = useAuth()
+
+    const getRoutingElement = ({ route, currentPath, submenu }: getRoutingProps): JSX.Element => {
+        if (currentPath === '/login' && userInfo?.name) {
+            return <HomePage />
+        } else if (currentPath === '/admin' && adminInfo?.userName) {
+            return <DashboardPlanDetails />
+        } else if (currentPath.indexOf('/admin') === 0) {
+            if (adminInfo?.email) {
+                return <route.component currentPath={route.to} subMenu={route.subMenu} />
+            } else {
+                return <AdminLoginPage />;
+            }
+        } else if (currentPath.indexOf('/user') === 0) {
+            if (userInfo?.name) {
+                return <route.component currentPath={route.to} subMenu={route.subMenu} />
+            } else {
+                return <Login />;
+            }
+        } else {
+            return <route.component currentPath={route.to} subMenu={route.subMenu} />;
+        }
+    }
+
     switch (routingService.status) {
         case ServiceStatus.Init:
         case ServiceStatus.Loading:
@@ -24,13 +53,7 @@ export const AppRouting = ({
                         <Route
                             key={`${route.id}-route`}
                             path={route.path}
-                            element={
-                                route.to.indexOf('/admin') === 0 ?
-                                    adminInfo?.email ? <route.component currentPath={route.to} subMenu={route.subMenu} /> : <AdminLoginPage />
-                                    : route.to.indexOf('/user') === 0 ?
-                                        userInfo?.name ? <route.component currentPath={route.to} subMenu={route.subMenu} /> : <Login />
-                                        : <route.component currentPath={route.to} subMenu={route.subMenu} />
-                            }
+                            element={getRoutingElement({ route: route, currentPath: route.to, submenu: route.subMenu })}
                         />
                     ))
                 }
