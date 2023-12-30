02a71bf4d7bf6e158f25d5ec84f2e48a57b32011
DESKTOP-PF4TAE2\hp <mauryashubham302@gmail.com>
2023-12-30 12:42:56 +0530

    allow access issue cors

--- api/routes.php
+++ api/routes.php
@@ -14,16 +14,16 @@ require_once('./customer-kyc.php');
 $method = $_SERVER['REQUEST_METHOD'];
 $helper = new Helper();
 //*****************Allow cross origion******************** */
-//header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Max-Age: 86400');
-// if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-// 	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-// 		header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS, DELETE");
-// 	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-// 		header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-// 	exit(0);
-// }
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+		header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS, DELETE");
+	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+		header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+	exit(0);
+}
 //*****************Allow cross origion******************** */
 try {
 	@$bodyRawData = json_decode(file_get_contents('php://input'), true);
